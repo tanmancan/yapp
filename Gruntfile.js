83abe0fb9d9ac2af80d4c5cc308ffab7bcfe6522
tanmancan <contact@tkarimdesign.com>
2017-02-24 19:19:57 -0500

    formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,62 +1,62 @@
 module.exports = function(grunt) {
-	'use strict';
+  'use strict';
 
-	// Plugins
-	require('load-grunt-tasks')(grunt);
+  // Plugins
+  require('load-grunt-tasks')(grunt);
 
-	// Config
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		jshint: {
-	    	all: ['Gruntfile.js', 'js/yapp.js']
-		},
-		uglify: {
-			options: {
-				mangle: true,
-				banner: '/*! <%= pkg.name %> - v<%= pkg.version %>\n' +
-        				'The MIT License (MIT)\n' +
-        				'Copyright (c) 2015 Tanveer Karim\n' +
-						'http://www.tkarimdesign.com/\n' +
-						'https://github.com/tanmancan/yapp\n' +
-						'*/\n'
-			},
-			dist: {
-		      files: {
-		        'js/yapp.min.js': ['js/yapp.js']
-		      }
-		    }
-		},
-		//Compile SASS
+  // Config
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+        all: ['Gruntfile.js', 'js/yapp.js']
+    },
+    uglify: {
+      options: {
+        mangle: true,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %>\n' +
+                'The MIT License (MIT)\n' +
+                'Copyright (c) 2015 Tanveer Karim\n' +
+            'http://www.tkarimdesign.com/\n' +
+            'https://github.com/tanmancan/yapp\n' +
+            '*/\n'
+      },
+      dist: {
+          files: {
+            'js/yapp.min.js': ['js/yapp.js']
+          }
+        }
+    },
+    //Compile SASS
         sass: {
             dist: {
-                options: { 
+                options: {
                     outputStyle: 'compressed',
                     sourceMap: false
                 },
-                files: { 
+                files: {
                     'demo/demo.css': 'demo/demo.scss'
                 }
             }
         },
-		// Watch
+    // Watch
         watch: {
-	        sass: {
-	            files: ['demo/*.scss'],
-	            tasks: ['sass:dist']
-	        },
+          sass: {
+              files: ['demo/*.scss'],
+              tasks: ['sass:dist']
+          },
             other: {
                 files: ['*.html', 'js/*.js']
             },
             // Live reload on file changes
-            options: { 
-                livereload: true 
+            options: {
+                livereload: true
             }
         },
-	});
+  });
 
-	// Task(s)
-    grunt.registerTask('dev', ['watch']);
-	grunt.registerTask('test', ['jshint']);
-	grunt.registerTask('default', ['uglify']);
+  // Task(s)
+  grunt.registerTask('dev', ['watch']);
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('default', ['uglify']);
 
 };
